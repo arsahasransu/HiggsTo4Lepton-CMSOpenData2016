98dc277160079376ad5a45720f0de6475f4a9b29
Abanti Ranadhir Sahasransu <a.r.sahasransu@cern.ch>
2026-01-05 16:43:32 +0000

    Few changes for event storage

--- 4mu_analyser.py
+++ 4mu_analyser.py
@@ -234,7 +234,7 @@ def analyse_4mu_data(input_file, output_file, lumi_json_path="", save_snapshot_p
     histograms.append(df_4muM.Histo1D(("h_muon_4MuM", "Muon M; M (GeV/c); Events", 250, 0, 500), "M4Mu"))
 
     # Keep the higgs event details for later
-    df_4muM = df_4muM.Filter("M4Mu > 70")
+    df_4muM = df_4muM.Filter("M4Mu > 0")
     if save_snapshot_path is not None:
         cols_to_keep = ["run", "luminosityBlock", "event", "M4Mu"]
         df_4muM.Snapshot("Events", f"{save_snapshot_path}.root", cols_to_keep)
